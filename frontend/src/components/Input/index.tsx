8984a26c73c0505f81806780984e8b41348a69a8
leye195 <leye195@naver.com>
2023-07-24 19:00:22 +0900

    feat: apply classnames on Input

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,10 +1,17 @@
 import { ComponentPropsWithRef, forwardRef } from "react";
 import { inputStyle } from "./style.css";
+import { classnames } from "@/lib/utils";
 
 type Props = ComponentPropsWithRef<"input">;
 
 const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  return <input {...props} ref={ref} className={inputStyle} />;
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className={classnames(props.className ?? "", inputStyle)}
+    />
+  );
 });
 
 export default Input;

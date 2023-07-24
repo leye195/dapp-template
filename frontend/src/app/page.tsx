968eff374d87e1eed95166fd40ea81165f933af8
leye195 <leye195@naver.com>
2023-07-24 19:42:45 +0900

    feat: set wallet connect buttons on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import useWallet from "@/hooks/useWallet";
+
 import Button from "@/components/Button";
 import Flex from "@/components/Flex";
 
 export default function Home() {
+  const { address, isConnected, connectors, connect, disconnect } = useWallet();
+
   return (
     <main>
-      <Flex width="100%" justifyContent="center">
-        main
+      <Flex flexDirection="column" alignItems="center">
+        <Flex width="100%" justifyContent="center">
+          <h1>Dapp Template</h1>
+        </Flex>
+        {isConnected && <p>{address}</p>}
+        <Flex flexDirection="column" alignItems="center" gap="8px">
+          {!isConnected ? (
+            <>
+              {connectors.map((connector) => (
+                <Button
+                  key={connector.id}
+                  onClick={() => connect({ connector })}
+                >
+                  Connect {connector.name}
+                </Button>
+              ))}
+            </>
+          ) : (
+            <Button onClick={() => disconnect()}>Disconnect</Button>
+          )}
+        </Flex>
       </Flex>
-      <Button>ttt</Button>
-      <Button padding="8px">ttt</Button>
     </main>
   );
 }
